290946348e34c07c78e98a657aab6d9b62e3bbd8
rizkysr90 <rizkysr90@gmail.com>
2021-09-21 14:33:33 +0700

    add echo framework,gorm,and make config,model package

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"project/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := "root:adarizki123@tcp(127.0.0.1:3306)/library?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+	InitMigrate()
+}
+
+func InitMigrate() {
+	DB.AutoMigrate(&model.User{})
+}
---
+++ model/model.go
@@ -0,0 +1,9 @@
+package model
+
+type User struct {
+	ID       int    `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	Address  string `json:"address" form:"address"`
+}
