a07fd6723bc1f1fecbcc4b992d0a53611504461b
Rizky Ramadhan <rizkysr90@gmail.com>
2021-10-07 01:20:48 +0700

    init ci unit test

---
+++ constant/constant.go
@@ -0,0 +1,4 @@
+package constant
+
+const JWT_SECRET_ADMIN = "secretadmin123!!!!"
+const JWT_SECRET_USER = "secretuser123!!!!!"
---
+++ mvc/constant/constant.go
@@ -0,0 +1,4 @@
+package constants
+
+const JWT_SECRET_USER = "rizki123"
+const JWT_SECRET_ADMIN = "admin123"
